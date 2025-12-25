2c6f2e0273a3c088cb4dae4f0eb5361b80b59f06
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-12-25 12:23:57 +0530

    Prepare for WP.org release

--- really-simple-featured-image.php
+++ really-simple-featured-image.php
@@ -25,66 +25,6 @@ define( 'RS_FEATURED_IMAGE_PLUGIN_DIR', plugin_dir_path( RS_FEATURED_IMAGE_PLUGI
 define( 'RS_FEATURED_IMAGE_PLUGIN_BASE', plugin_basename( RS_FEATURED_IMAGE_PLUGIN_FILE ) );
 define( 'RS_FEATURED_IMAGE_PLUGIN_PRO_URL', 'https://jetixwp.com/plugins/really-simple-featured-image' );
 
-// Third party dependencies.
-$rs_featured_image_vendor_file = __DIR__ . '/vendor/autoload.php';
-
-if ( is_readable( $rs_featured_image_vendor_file ) ) {
-	require_once $rs_featured_image_vendor_file;
-}
-
-/**
- * Include and initialize the updater.
- */
-require_once RS_FEATURED_IMAGE_PLUGIN_DIR . 'includes/class-updater.php';
-\RS_Featured_Image\Updater::init_updater();
-
-/**
- * Initialize Freemius SDK.
- */
-if ( ! function_exists( 'rs_featured_image_fs' ) ) {
-	/**
-	 * Create a helper function for easy SDK access.
-	 */
-	function rs_featured_image_fs() {
-		global $rs_featured_image_fs;
-
-		if ( ! function_exists( 'fs_dynamic_init' ) && file_exists( __DIR__ . '/vendor/freemius/wordpress-sdk/start.php' ) ) {
-			require_once __DIR__ . '/vendor/freemius/wordpress-sdk/start.php';
-		}
-
-		if ( ! isset( $rs_featured_image_fs ) && function_exists( 'fs_dynamic_init' ) ) {
-			$rs_featured_image_fs = fs_dynamic_init(
-				array(
-					'id'             => '22490',
-					'slug'           => 'really-simple-featured-image',
-					'type'           => 'plugin',
-					'public_key'     => 'pk_cdc4b578d06509291b1e11d9339cf',
-					'is_premium'     => false,
-					'has_addons'     => false,
-					'has_paid_plans' => false,
-					'menu'           => array(
-						'slug'       => 'rs-featured-image-settings',
-						'first-path' => 'admin.php?page=rs-featured-image-settings',
-						'support'    => false,
-						'account'    => false,
-						'contact'    => false,
-						'parent'     => array(
-							'slug' => 'jetixwp',
-						),
-					),
-				)
-			);
-		}
-
-		return $rs_featured_image_fs;
-	}
-
-	// Init Freemius.
-	rs_featured_image_fs();
-	// Signal that SDK was initiated.
-	do_action( 'rs_featured_image_fs_loaded' );
-}
-
 /**
  * Fire up plugin instance.
  */
