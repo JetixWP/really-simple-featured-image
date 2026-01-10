e61137db8f860a70178656e97eb3cfab4113776b
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2026-01-10 08:03:51 +0530

    Add vendor includes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jetixwp/really-simple-featured-image',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7f57662338857ab72672919146e3812bb8771056',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.13.0',
+            'version' => '2.13.0.0',
+            'reference' => '3cbe98b5bd0b0fb5ca4df97b8088592737ea4375',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jetixwp/really-simple-featured-image' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7f57662338857ab72672919146e3812bb8771056',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
